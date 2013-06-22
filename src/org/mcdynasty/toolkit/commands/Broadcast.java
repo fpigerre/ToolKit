f537b70a7f329900127b4108997af9a387885c2c
Francois Pigerre <fpigerre@gmail.com>
2013-06-22 17:30:22 +1000

    Removed sk89q command framework for some commands
    Removed sk89q command framework for broadcast and staffchat commands.

--- src/org/mcdynasty/toolkit/commands/Broadcast.java
+++ src/org/mcdynasty/toolkit/commands/Broadcast.java
@@ -3,33 +3,22 @@ package org.mcdynasty.toolkit.commands;
 import org.bukkit.command.*;
 import org.bukkit.ChatColor;
 import org.bukkit.Bukkit;
-import org.mcdynasty.toolkit.ToolKit;
-
-import com.sk89q.minecraft.util.commands.*;
-import com.sk89q.minecraft.util.commands.Command;
-import com.sk89q.minecraft.util.commands.CommandException;
 
 public class Broadcast {
 
 	ChatColor AQUA = ChatColor.AQUA;
 	
-	ToolKit plugin;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("broadcast")) {
+			if (args.length == 0) {
+				sender.sendMessage(AQUA + "Please specify a message to broadcast!");
+				sender.sendMessage(AQUA + "Correct usage: /broadcast <message>");
 
-	public Broadcast(ToolKit pl) {
-		plugin = pl;
-	}
-
-	@Command(aliases = { "broadcast" }, usage = "/broadcast <message>", desc = "Broadcasts a command to the entire server.")
-	@CommandPermissions({ "toolkit.broadcast" })
-	public void broadcast(final CommandContext args, CommandSender sender)
-			throws CommandException {
-		if (args.argsLength() == 0) {
-			sender.sendMessage(AQUA + "Please specify a message to broadcast!");
-			sender.sendMessage(AQUA + "Correct usage: /broadcast <message>");
-
-		} else {
-			Bukkit.broadcastMessage("[" + ChatColor.GOLD + "BROADCAST"
-					+ ChatColor.WHITE + "] " + args);
+			} else {
+				Bukkit.broadcastMessage("[" + ChatColor.GOLD + "BROADCAST"
+						+ ChatColor.WHITE + "] " + args);
+			}
 		}
+		return false;
 	}
 }
--- src/org/mcdynasty/toolkit/commands/StaffChat.java
+++ src/org/mcdynasty/toolkit/commands/StaffChat.java
@@ -4,37 +4,34 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import com.sk89q.minecraft.util.commands.Command;
-import com.sk89q.minecraft.util.commands.CommandContext;
-import com.sk89q.minecraft.util.commands.CommandException;
-import com.sk89q.minecraft.util.commands.CommandPermissions;
+import org.bukkit.command.*;
 
 public class StaffChat {
 
 	ChatColor AQUA = ChatColor.AQUA;
 	ChatColor RED = ChatColor.RED;
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("staffchat")) {
+			if (sender.hasPermission("toolkit.staffchat")) {
+			if (args.length == 0) {
+				sender.sendMessage(AQUA + "Please specify a message to broadcast!");
+				sender.sendMessage(AQUA + "Correct usage: /staffchat <message>");
 
-	@Command(aliases = { "staffchat", "sc", "chat" }, usage = "/staffchat <message>", desc = "Broadcasts a message to all online staff.")
-	@CommandPermissions({ "toolkit.staffchat" })
-	public void staffchat(final CommandContext args, CommandSender sender)
-			throws CommandException {
-		if (args.argsLength() == 0) {
-			sender.sendMessage(AQUA + "Please specify a message to broadcast!");
-			sender.sendMessage(AQUA + "Correct usage: /staffchat <message>");
-
-		} else {
-
-			final String scmessage = (RED + "[StaffChat]: " + ChatColor.RESET
-					+ sender.getName() + " " + args);
-			for (final Player p : Bukkit.getOnlinePlayers()) {
-				{
-					if (p.hasPermission("toolkit.staffchat.receive")) {
-						continue;
+			} else {
+				final String scmessage = (RED + "[StaffChat]: " + ChatColor.RESET
+						+ sender.getName() + " " + args);
+				for (final Player p : Bukkit.getOnlinePlayers()) {
+					{
+						if (p.hasPermission("toolkit.staffchat.receive")) {
+							continue;
+						}
+						p.sendMessage(scmessage);
 					}
-					p.sendMessage(scmessage);
 				}
 			}
+			}
 		}
+		return false;
 	}
 }
