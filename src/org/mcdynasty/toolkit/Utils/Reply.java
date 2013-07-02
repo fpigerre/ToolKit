e2651a9e1f4f5b14ee5de944e9d5a978c1feae12
Francois Pigerre <fpigerre@gmail.com>
2013-07-02 13:52:54 +1000

    Remove sk89q framework again

--- src/org/mcdynasty/toolkit/Utils/Reply.java
+++
@@ -1,9 +0,0 @@
-package org.mcdynasty.toolkit.Utils;
-
-import org.bukkit.command.CommandSender;
-
-public interface Reply {
-	void setReplyTo(CommandSender user);
-
-	CommandSender getReplyTo();
-}
